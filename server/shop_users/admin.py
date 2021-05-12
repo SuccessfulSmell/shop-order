3e1574c358280fa9a6d07cec5ec4ae785cc3fc19
nikita <18nikita99@gmail.com>
2021-05-12 21:20:04 +0300

    Add personal room configs.

--- server/shop_users/admin.py
+++ server/shop_users/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
+from .models import UserDiscount, ProductsInOrder, UserOrder
 
-# Register your models here.
+PER_PAGE = 10
 
+
+class UserDiscountAdmin(admin.ModelAdmin):
+    search_fields = ['user', 'discount']
+    list_display = ('user', 'discount')
+    list_per_page = PER_PAGE
+
+
+class ProductsInOrderAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'user', 'product_count', 'product']
+    list_display = ('id', 'user', 'product_count', 'product')
+    list_per_page = PER_PAGE
+
+
+class UserOrderAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'user', 'total_price', 'date']
+    list_display = ('id', 'user', 'total_price', 'date')
+    list_per_page = PER_PAGE
+
+
+admin.site.register(UserDiscount, UserDiscountAdmin)
+admin.site.register(ProductsInOrder, ProductsInOrderAdmin)
+admin.site.register(UserOrder, UserOrderAdmin)
