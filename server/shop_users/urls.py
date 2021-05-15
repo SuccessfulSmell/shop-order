e420061776a71ef03baa2ee977b74882150d61cb
nikita <18nikita99@gmail.com>
2021-05-15 16:52:30 +0300

    Add change user

--- server/shop_users/urls.py
+++ server/shop_users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import new_order, ChangePasswordView
+from .views import new_order, ChangePasswordView, UpdateProfileView
 
 urlpatterns = [
     path('add_order/', new_order),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
 ]
