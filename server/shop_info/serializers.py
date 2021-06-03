feb5c776b91ef323126266e2fb819e4217299113
nikita <18nikita99@gmail.com>
2021-06-03 23:19:36 +0300

    Add email

--- server/shop_info/serializers.py
+++ server/shop_info/serializers.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = SiteSettings
-        fields = ['title', 'contact_phone', 'logist_phone', 'address', 'about_info']
+        fields = ['title', 'contact_phone', 'logist_phone', 'address', 'about_info', 'email']
 
 
 class SalesSerializer(serializers.ModelSerializer):
